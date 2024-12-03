aac10203a8a6ca374e9b3a188b6b340ffa0c8850
Cyber <blacy1551@gmail.com>
2024-12-02 18:40:53 -0600

    first major release

--- src/main/java/com/cyber/fluidic_arm/FluidicArmMod.java
+++ src/main/java/com/cyber/fluidic_arm/FluidicArmMod.java
@@ -21,7 +21,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class FluidicArmMod {
 	public static final String MODID = "fluidic_arm";
 	public static final String NAME = "Fluidic Arm";
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "1.0.0";
 
 	private static Logger logger;
 
